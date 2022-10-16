673dfd388618ac19bd4ab70480011f808ca92da0
unknown <haib6273@gmail.com>
2022-10-16 22:25:03 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,19 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Document</title>
+    <style>
+        body{
+            background-image: url("https://images.unsplash.com/photo-1493723843671-1d655e66ac1c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80");
+        }
+        h1{
+            font-size: 15rem;
+
+        }
+        .btn-outline-primary{
+            font-size: 3rem;
+            background-color: #0000001c;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
